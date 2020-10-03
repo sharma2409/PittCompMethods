76c6012d98f8bdea0b4cf9d6a5607262e4162149
Raghav Sharma <Raghav Sharma>
2020-10-03 13:50:53 -0400

    Adding new files

--- CH5/EX6/EX6.cpp
+++ CH5/EX6/EX6.cpp
@@ -1,17 +1,43 @@
-#include "QatGenericFunctions/NormalDistribution.h"
+#include <eigen3/Eigen/Dense>
 #include "QatGenericFunctions/GaussQuadratureRule.h"
 #include "QatGenericFunctions/GaussIntegrator.h"
-
+#include "QatGenericFunctions/HermitePolynomial.h"
+#include "QatGenericFunctions/Exp.h"
+#include "QatGenericFunctions/Square.h"
+#include <iostream>
+#include <cmath>
 int main(int argc, char**argv){
 
 using namespace Genfun;
-NormalDistribution f;
+using namespace Eigen;
 
-GaussLegendreRule rule(argc==2 ? std::stoi(argv[1]):4);
+MatrixXf mat(6,6);
+
+GaussHermiteRule rule(6);
 GaussIntegrator integrator(rule);
 
-double approx=integrator(f);
-std::cout<<approx;
+double func;
+
+for (int i=0; i<6; i++){
+
+for (int j=0; j<6; j++)
+{
+
+Exp exp;
+Square square;
+GENFUNCTION H=exp(-square/2.0);
+
+
+func=sqrt(pow(2,i)*tgamma(i+1)*pow(2,j)*tgamma(j+1));
+
+GENFUNCTION phi=(sqrt(M_PI)/func)*H*HermitePolynomial(i)*HermitePolynomial(j);
+
+mat(i,j)=integrator(phi);
+
+}
+};
+
+std::cout<<mat<<std::endl;
 
 return 0;
 }
