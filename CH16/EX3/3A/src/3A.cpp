3ee518a68433c9250e22fde5282d94e4aa3a906b
Raghav Sharma <Raghav Sharma>
2020-11-29 17:25:49 -0500

    Add Existing Files

--- CH16/EX3/3A/src/3A.cpp
+++ CH16/EX3/3A/src/3A.cpp
@@ -1,5 +1,6 @@
 #include "QatPlotWidgets/PlotView.h"
 #include "QatPlotting/PlotStream.h"
+#include "QatPlotting/PlotFunction1D.h"
 #include <QApplication>
 #include <QMainWindow>
 #include <QToolBar>
@@ -11,14 +12,20 @@
 #include "QatDataAnalysis/Hist1D.h"
 #include "QatPlotting/PlotHist1D.h"
 #include "QatDataModeling/HistChi2Functional.h"
+#include "QatDataModeling/MinuitMinimizer.h"
 #include <cmath>
 #include "QatGenericFunctions/Variable.h"
+#include "QatGenericFunctions/Exp.h"
+#include "QatGenericFunctions/Sqrt.h"
 #include "QatGenericFunctions/Parameter.h"
+#include "Eigen/Dense"
 using namespace std;
 using namespace Genfun;
 
 
 int main (int argc, char * * argv) {
+
+
 
   // Automatically generated:-------------------------:
 
@@ -35,11 +42,14 @@ int main (int argc, char * * argv) {
   
   QObject::connect(quitAction, SIGNAL(triggered()), &app, SLOT(quit()));
   
-  Hist1D histogram (1000.0, 80.0, 110.0);
+  Hist1D histogram (1000.0, 90.0, 110.0);
   
   ifstream stream("data00.dat");
   
   double x;
+  
+  Exp exp;
+  Sqrt sqrt;
   
   while (stream>>x) {
     
@@ -47,13 +57,26 @@ int main (int argc, char * * argv) {
   }
   
   Parameter pgamma("Gamma",0.5,0.01,4.0);
+  //Parameter psigma("Sigma", 0.5,0.01,10.0);
   
   Variable X;
   
-  GENFUNCTION lorenzianDistribution=1/(M_PI*pgamma*(1+(X/pgamma)*(X/pgamma)));
+  GENFUNCTION lorenzianDistribution=1/(M_PI*pgamma*(1+(X/pgamma)*(X/pgamma))); //Function for Lorentzian Distribution
+    //GENFUNCTION normaldistribution=exp((-X*X)/(2*psigma*psigma))/sqrt(2*M_PI)/psigma; 
  
   HistChi2Functional objectiveFunction(&histogram);
-
+  GENFUNCTION f= histogram.sum()*lorenzianDistribution;
+  //GENFUNCTION f= histogram.sum()*normaldistribution;
+  
+  bool verbose=true;
+  MinuitMinimizer minimizer(verbose);
+  minimizer.addParameter(&pgamma);
+ // minimizer.addParameter(&psigma);
+  minimizer.addStatistic(&objectiveFunction, &f);
+  minimizer.minimize();
+  
+  //PlotFunction1D pplot(histogram.sum()*lorenzianDistribution); 
+  
   PlotHist1D pH=histogram;
   PlotView view(pH.rectHint());
   view.setGrid(false);
@@ -61,6 +84,7 @@ int main (int argc, char * * argv) {
   view.setXZero(false);
   view.setYZero(false);
   view.add(&pH);
+  view.add(&pplot);
   view.yAxisFont().setPointSize(12);
   window.setCentralWidget(&view);
 
